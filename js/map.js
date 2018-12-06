51c8034742a12934f5d97e07ba554e264f0ce0c1
carlos <carlos.castaneda.gutierrez@gmail.com>
2018-12-05 19:31:14 -0500

    Remove unnecesary UI components from Map

--- js/map.js
+++ js/map.js
@@ -6,10 +6,10 @@ function codeAddress() {
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == 'OK') {
         map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
+        /*var marker = new google.maps.Marker({
             map: map,
             position: results[0].geometry.location
-        });
+        });*/
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
       }
@@ -20,7 +20,8 @@ function initMap() {
     geocoder = new google.maps.Geocoder();
     map = new google.maps.Map(document.getElementsByClassName('map-content')[0], {
         center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
+        zoom: 8,
+        disableDefaultUI: true
     });
 
     // Center map to address (city, COUNTRY) 
